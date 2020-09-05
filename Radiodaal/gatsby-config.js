1e6e573d6877f4b112b0c7f7b5a7db0a7ffa3d97
alipajand <ali.pajand@gmail.com>
2020-09-05 14:42:33 +0430

    :art: Create sample html files

---
+++ Radiodaal/gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+  ],
+}
